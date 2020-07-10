b5dffabe1d11ef3ede6ba1c02d23cffd60b01954
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-10 15:07:41 -0400

    feat: set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
 // Components
 import BookSearch from "./components/BookSearch";
 import BookCase from "./components/BookCase";
@@ -8,15 +9,6 @@ import "./App.css";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     *
-     * TODO: (TL;DR) Implement React Router instead of state for showing the search page...
-     */
-    showSearchPage: false,
     books: [],
     bookShelves: ["Currently Reading", "Want To Read", "Read"] // Here for future improvements... TODO: add ability to create new shelves
   };
@@ -53,15 +45,17 @@ class BooksApp extends React.Component {
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-        {this.state.showSearchPage ? (
-          <BookSearch />
-        ) : (
-          <BookCase
-            books={this.state.books}
-            shelves={this.state.bookShelves}
-            updateBook={this.updateBook}
-          />
-        )}
+          <Route path="/search">
+            <BookSearch />
+          </Route>
+          <Route exact path ="/">
+            <BookCase
+              books={this.state.books}
+              shelves={this.state.bookShelves}
+              updateBook={this.updateBook}
+            />
+          </Route>
+        
       </div>
     );
   }
