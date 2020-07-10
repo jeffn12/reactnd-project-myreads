cf3ed8d4aa91611c81cf86c82346aa7ca48dd119
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-10 10:07:30 -0400

    feat: update bookshelf to map through a given array of books

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 import Book from './Book.js';
 
+const books = [
+  {
+    author: 'Harper Lee',
+    title: 'To Kill A Mockingbird',
+    url:
+      'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+  },
+  {
+    author: 'Orson Scott Card',
+    title: "Ender's Game",
+    url:
+      'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
+  }
+];
+
 export class BookShelf extends Component {
   render() {
     return (
@@ -9,20 +24,11 @@ export class BookShelf extends Component {
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book
-                title="To Kill A Mockingbird"
-                author="Harper Lee"
-                url="http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
-              />
-            </li>
-            <li>
-              <Book
-                title="Ender's Game"
-                author="Orson Scott Card"
-                url="http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
-              />
-            </li>
+            {books.map((book) => (
+              <li>
+                <Book title={book.title} author={book.author} url={book.url} />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
