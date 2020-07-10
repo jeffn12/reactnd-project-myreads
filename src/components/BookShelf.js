889691d61efa4e062fc95de6c00e1805eef193fc
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-10 10:24:52 -0400

    feat: make bookshelf reusable with props

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,29 +2,18 @@ import React, { Component } from 'react';
 
 import Book from './Book.js';
 
-const books = [
-  {
-    author: 'Harper Lee',
-    title: 'To Kill A Mockingbird',
-    url:
-      'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
-  },
-  {
-    author: 'Orson Scott Card',
-    title: "Ender's Game",
-    url:
-      'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
-  }
-];
+/*
+ *   BookShelf Class - component that represents a shelf in the bookcase.  A shelf holds the books that are associated with its category
+ */
 
 export class BookShelf extends Component {
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {books.map((book) => (
+            {this.props.books.map((book) => (
               <li>
                 <Book title={book.title} author={book.author} url={book.url} />
               </li>
