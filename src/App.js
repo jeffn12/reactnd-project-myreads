09f82ad3276ff66fe94f31bdad1009c72c2e8a7b
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-10 09:43:37 -0400

    Style: adjust tab spacing

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import BookCase from './components/BookCase';
 import './App.css';
 
 class BooksApp extends React.Component {
-	state = {
-		/**
-		 * TODO: Instead of using this state variable to keep track of which page
-		 * we're on, use the URL in the browser's address bar. This will ensure that
-		 * users can use the browser's back and forward buttons to navigate between
-		 * pages, as well as provide a good URL they can bookmark and share.
-		 */
-		showSearchPage: false,
-	};
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  };
 
-	render() {
-		return (
-			<div className="app">
-				{this.state.showSearchPage ? <BookSearch /> : <BookCase />}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="app">
+        {this.state.showSearchPage ? <BookSearch /> : <BookCase />}
+      </div>
+    );
+  }
 }
 
 export default BooksApp;
