4d5ab0cb80266e31ceff024fd81b07c535f27995
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-10 19:30:30 -0400

    feat: add proptypes

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 // Components
 import BookShelf from "./BookShelf";
 
@@ -10,6 +11,10 @@ import * as BooksAPI from "../BooksAPI";
  */
 
 export class BookSearch extends Component {
+  static propTypes = {
+    updateBook: PropTypes.func.isRequired
+  };
+
   state = {
     query: "",
     results: []
