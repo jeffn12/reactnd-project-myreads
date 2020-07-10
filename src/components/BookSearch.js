4cb9787bfd898af9fa94b563f1de2ee176c0cf31
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-10 15:48:08 -0400

    feat: connect search form to state

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import * as BooksAPI from "../BooksAPI";
 
 /*
  *   BookSearch Class - component for the search page
  */
 
 export class BookSearch extends Component {
+  state = {
+    query: "",
+    results: []
+  };
+
+  handleChange = (query) => {
+    this.setState(() => ({ query }));
+    this.getResults();
+  };
+
+  getResults = () => {
+    BooksAPI.search(this.state.query)
+      .then((books) => {
+        this.setState(() => ({ results: books }));
+      })
+      .catch((err) => console.error(err));
+  };
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/">
-            <button className="close-search">
-              Close
-            </button>
+            <button className="close-search">Close</button>
           </Link>
           <div className="search-books-input-wrapper">
             {/*
@@ -24,7 +42,12 @@ export class BookSearch extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(e) => this.handleChange(e.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
